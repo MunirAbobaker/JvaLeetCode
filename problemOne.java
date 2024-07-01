7db1d732cb12e32db53e26f9b6c4a24ee9d2b293
MunirAbobaker <munir.abobaker@sap.com>
2024-07-01 11:23:16 +0200

    solution first problem

---
+++ problemOne.java
@@ -0,0 +1,15 @@
+class Solution {
+    // using hashMap is more performant as using nested loop
+    // as the lookup key is in constant time
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        return new int[] {};
+    }
+}
